84e8c90d7d6946a41500b7d80483fc49e8660c62
703joko <703joko@gmail.com>
2023-07-24 12:34:13 +0700

    Initial commit

---
+++ app/src/main/java/com/jrmod/mycoaxs/MainActivity.java
@@ -0,0 +1,96 @@
+package com.jrmod.mycoaxs;
+
+import static java.lang.Math.sqrt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button button1;
+    EditText input1, input2, input3, input4;
+    TextView hasil1, x, y, z, c, justment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        input1 = findViewById(R.id.input1);
+        input2 = findViewById(R.id.input2);
+        input3 = findViewById(R.id.input3);
+        input4 = findViewById(R.id.input4);
+
+        button1 = findViewById(R.id.button1);
+        hasil1 = findViewById(R.id.hasil1);
+        justment = findViewById(R.id.justy);
+        x = findViewById(R.id.hsil1);
+        y = findViewById(R.id.hsil2);
+        z = findViewById(R.id.hsilA);
+        c = findViewById(R.id.hsilB);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                try {
+                    double pertama, kedua, ketiga, keempat, hsl1, hsl2, hslA, hslB, A, X, final1;
+                    NumberFormat nf = NumberFormat.getInstance();
+                    pertama = Double.parseDouble(input1.getText().toString());
+                    kedua = Double.parseDouble(input2.getText().toString());
+                    ketiga = Double.parseDouble(input3.getText().toString());
+                    keempat = Double.parseDouble(input4.getText().toString());
+                    hsl1 = pertama - ketiga;
+                    hsl2 = kedua - keempat;
+                    hslA = hsl1 * hsl1;
+                    hslB = hsl2 * hsl2;
+                    A = hslA + hslB;
+                    X = Math.sqrt(A);
+                    final1 = X * 2;
+
+                    DecimalFormat df = new DecimalFormat("#.######");
+                    DecimalFormat formater = new DecimalFormat("#.###");
+                    String hsl1akhir = df.format(hsl1);
+                    String hsl2akhir = df.format(hsl2);
+                    String hslAakhir = df.format(hslA);
+                    String hslBakhir = df.format(hslB);
+                    String akhir = formater.format(final1);
+
+                    hasil1.setText(akhir);
+                    if (final1 >= 0.020) {
+                        justment.setText("Not Good");
+                        justment.setBackgroundColor(Color.RED);
+                        hasil1.setTextColor(Color.RED);
+                    } else if (final1 >= 0.014) {
+                        justment.setText("Out CheckSheet");
+                        justment.setBackgroundColor(Color.BLUE);
+                        hasil1.setTextColor(Color.BLUE);
+                    } else {
+                        justment.setText("OK");
+                        justment.setBackgroundColor(Color.WHITE);
+                        hasil1.setTextColor(Color.BLACK);
+                    }
+
+                    x.setText(hsl1akhir);
+                    y.setText(hsl2akhir);
+                    z.setText(hslAakhir);
+                    c.setText(hslBakhir);
+                } catch (NumberFormatException ne) {
+                    hasil1.setText("Input dulu");
+                }
+            }
+
+
+        });
+    }
+}
